72fd4bba62d48056e2ca703fbabb5fb6e1f689fd
Adam Mitz <mitza@objectcomputing.com>
2023-01-20 18:21:41 -0600

    Build scripts and GitHub Actions updates

--- idl/build-libs.cmd
+++ idl/build-libs.cmd
@@ -5,6 +5,6 @@ mkdir build
 cd build
 copy ..\Message.idl
 copy ..\Message.mpc
-%ACE_ROOT%\bin\generate_export_file.pl Message > Message_Export.h
-%ACE_ROOT%\bin\mwc.pl -type vs2022
+perl %ACE_ROOT%\bin\generate_export_file.pl Message > Message_Export.h
+perl %ACE_ROOT%\bin\mwc.pl -type vs2022
 msbuild -p:Configuration=Debug,Platform=x64 -m build.sln
